ec1876c37efcc9e2a114f8d6ec3db1a3b4a1e8bb
bdstn10 <budistn108@gmail.com>
2024-10-29 19:21:33 +0700

    menambahkan config cors agar api dapat diakses dari app lain

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from database.database import init_db
 from api.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 @app.on_event('startup')
 async def startup():
     init_db(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(router=router)
